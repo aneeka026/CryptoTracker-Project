114738648b6d0ca64a4ac6ca571d4022b23eacc7
Aneeka <182370815+aneeka026@users.noreply.github.com>
2025-07-24 12:53:01 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Header = ({toggleTheme}) => {
   return (
     <header>
       <div><strong>Cryptoplace</strong></div>
       <nav>
-        <a href="#home">Home</a>
-        <a href="#features">Features</a>
-        <a href="#pricing">Pricing</a>
-        <a href="#blog">Blog</a>
-        <a href="#signup">Sign up</a>
+        <Link to="/">Home</Link>
+        <Link to="/features">Features</Link>
+        <Link to="/pricing">Pricing</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/signup">Sign up</Link>
       </nav>
 
-      {/* Theme toggle button: when clicked, it toggles between light and dark modes */}
       <button className="btn-toggle" onClick={toggleTheme}>
         🌙 Toggle Mode
       </button>
